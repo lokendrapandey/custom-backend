ff620f520da3333a41753bd14b54fa849cec88a2
lokendra pandey <lokendrapandey5278@gmail.com>
2026-06-15 07:22:21 +0530

    database connected

--- src/index.js
+++ src/index.js
@@ -9,17 +9,31 @@ dotenv.config({
 
 
 connectDB()
-.then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err) => {
-    console.log("MONGO db connection failed !!! ", err);
-})
+
+
 
 
 // PORT=8000
 // MONGODB_URI=mongodb+srv://lokendra:lokendra123@cluster0.57czmnb.mongodb.net/?appName=Cluster0
 // #lokendra
 // #lokendra123
+
+
+// import mongoose from 'mongoose';
+// import express from 'express';
+
+// const app = express();
+// const PORT = 8000;
+// const MONGODB_URI = "mongodb://localhost:27017/test";
+
+// mongoose.connect(MONGODB_URI)
+//   .then(() => {
+//     console.log("MongoDB connected!");
+//     app.listen(PORT, () => {
+//       console.log(`Server running on port ${PORT}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.log("MONGODB connection FAILED", error);
+//     process.exit(1);
+//   });
